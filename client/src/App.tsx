ac2fd840c856f2716e076d01fc1147401c2608c4
Josh <pawn.314@icloud.com>
2026-07-01 18:56:51 -0500

    Initialize FusionTwin application shell

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div
+      style={{
+        background: "#0B0D13",
+        color: "#E6EEF8",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontFamily: "Arial, sans-serif",
+        fontSize: "3rem",
+      }}
+    >
+      FusionTwin
+    </div>
+  );
+}
+
+export default App;
